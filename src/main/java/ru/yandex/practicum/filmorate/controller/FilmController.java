b12acb6958de57d3c38c010c9e085ee889e79c49
Vladislav Kotletkin <drzhkn1@mail.ru>
2024-12-27 23:39:44 +0300

    feat: Реализован FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,73 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+
+    Map<Long, Film> films = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film create(@RequestBody Film film) {
+        if (isNotPassedValidation(film)) {
+            throw new ValidationException("Название фильма не может быть пустым!");
+        }
+
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+
+        return film;
+    }
+
+    @PutMapping
+    public Film update(@RequestBody Film film) {
+        if (isNotPassedValidation(film)) {
+            throw new ValidationException("Название фильма не может быть пустым!");
+        }
+
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    private boolean isNotPassedValidation(Film film) {
+        return !(isCorrectFilmName(film) && isCorrectDescription(film) && isCorrectReleaseDate(film) && isCorrectDuration(film));
+    }
+
+    private boolean isCorrectFilmName(Film film) {
+        return film.getName() == null || film.getName().isBlank();
+    }
+
+    private boolean isCorrectDescription(Film film) {
+        return film.getDescription().length() <= 200;
+    }
+
+    private boolean isCorrectReleaseDate(Film film) {
+        return film.getReleaseDate().isAfter(LocalDate.parse("1895-12-28"));
+    }
+
+    private boolean isCorrectDuration(Film film) {
+        return film.getDuration().isPositive();
+    }
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,30 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    Map<Long, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+
+    }
+
+    @PutMapping
+    public User update(@RequestBody User user) {
+
+    }
+}
