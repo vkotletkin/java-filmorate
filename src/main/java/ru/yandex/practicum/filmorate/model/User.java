872f18b875ab6920cca48af439e6b1f8d275ba05
Vladislav Kotletkin <drzhkn1@mail.ru>
2025-01-01 18:03:13 +0300

    refactor: Удалил не используемые импорты

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.PastOrPresent;
 import lombok.AccessLevel;
 import lombok.Builder;
