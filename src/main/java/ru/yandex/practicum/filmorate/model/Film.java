d0bc2e9f23dfc1c3a137c4bc4293660d81d09a74
Vladislav Kotletkin <drzhkn1@mail.ru>
2025-01-01 17:24:07 +0300

    feat: Реализованы тесты для валидации на FilmController

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -24,9 +25,11 @@ public class Film {
     @Size(max = 200)
     String description;
 
+    @NotNull
     @FilmCreationConstraint
     LocalDate releaseDate;
 
+    @NotNull
     @Min(1)
     Long duration;
 }
--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTests.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.utils.FilmDefinition;
 
+import java.time.LocalDate;
 import java.util.Set;
 
 public class FilmControllerTests {
@@ -33,9 +34,7 @@ public class FilmControllerTests {
     }
 
     @Test
-    public void testCreateFailName() {
-        film = FilmDefinition.filmFifthElement;
-
+    public void testCreateFailLogin() {
         film = Film.builder()
                 .id(FilmDefinition.filmFifthElement.getId())
                 .description(FilmDefinition.filmFifthElement.getDescription())
@@ -48,5 +47,47 @@ public class FilmControllerTests {
         Assertions.assertEquals(1, violations.size());
     }
 
+    @Test
+    public void testCreateFailDescription() {
+        film = Film.builder()
+                .id(FilmDefinition.filmFifthElement.getId())
+                .name(FilmDefinition.filmFifthElement.getName())
+                .releaseDate(FilmDefinition.filmFifthElement.getReleaseDate())
+                .duration(FilmDefinition.filmFifthElement.getDuration())
+                .build();
 
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+
+        Assertions.assertEquals(1, violations.size());
+    }
+
+    @Test
+    public void testCreateFailReleaseDate() {
+        film = Film.builder()
+                .id(FilmDefinition.filmFifthElement.getId())
+                .name(FilmDefinition.filmFifthElement.getName())
+                .releaseDate(LocalDate.parse("1500-01-01"))
+                .description(FilmDefinition.filmFifthElement.getDescription())
+                .duration(FilmDefinition.filmFifthElement.getDuration())
+                .build();
+
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+
+        Assertions.assertEquals(1, violations.size());
+    }
+
+    @Test
+    public void testCreateFailDuration() {
+        film = Film.builder()
+                .id(FilmDefinition.filmFifthElement.getId())
+                .name(FilmDefinition.filmFifthElement.getName())
+                .description(FilmDefinition.filmFifthElement.getDescription())
+                .releaseDate(FilmDefinition.filmFifthElement.getReleaseDate())
+                .duration(-200L)
+                .build();
+
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+
+        Assertions.assertEquals(1, violations.size());
+    }
 }
