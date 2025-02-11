a5038c7a177c7d15a3b3adc35be29c0825ffaf8c
Vladislav Kotletkin <drzhkn1@mail.ru>
2025-02-11 23:59:43 +0300

    refactor: some fixes

--- src/main/java/ru/yandex/practicum/filmorate/annotation/validator/FilmCreationLocalDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/annotation/validator/FilmCreationLocalDateValidator.java
@@ -7,10 +7,8 @@ import ru.yandex.practicum.filmorate.annotation.FilmCreationConstraint;
 import java.time.LocalDate;
 
 public class FilmCreationLocalDateValidator implements ConstraintValidator<FilmCreationConstraint, LocalDate> {
-
     @Override
     public boolean isValid(LocalDate localDate, ConstraintValidatorContext context) {
         return localDate.isAfter(LocalDate.parse("1895-12-28"));
     }
-
 }
