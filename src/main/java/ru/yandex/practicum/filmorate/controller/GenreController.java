bc27947541c2e8f4159b75c845a30b69887836f8
Vladislav Kotletkin <drzhkn1@mail.ru>
2025-02-14 00:57:30 +0300

    feat: added controllers scratch

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/genres")
+public class GenreController {
+}
