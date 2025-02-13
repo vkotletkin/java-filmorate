2ce8a5be39ccefa3d5e9042a92c554846f811b83
Vladislav Kotletkin <drzhkn1@mail.ru>
2025-02-13 20:39:20 +0300

    refactor: вынесены запросы в отдельный пакет, немного рефакторинг

--- src/main/java/ru/yandex/practicum/filmorate/dal/impl/H2DatabaseRelationDaoImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/impl/H2DatabaseRelationDaoImpl.java
@@ -3,104 +3,47 @@ package ru.yandex.practicum.filmorate.dal.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.dal.RelationDao;
 import ru.yandex.practicum.filmorate.dal.mapper.RelationRowMapper;
 import ru.yandex.practicum.filmorate.entity.Relation;
-import ru.yandex.practicum.filmorate.entity.User;
 
-import java.sql.PreparedStatement;
 import java.util.List;
-import java.util.Objects;
+
+import static ru.yandex.practicum.filmorate.dal.impl.query.RelationQuery.*;
 
 @Slf4j
 @Repository
 @RequiredArgsConstructor
 public class H2DatabaseRelationDaoImpl implements RelationDao {
 
-    private static final String queryCreateRelation = """
-            INSERT INTO FRIENDS_RELATIONS (user_id, friend_id, relation_status)
-            VALUES (?, ?, ?)
-            """;
-    private static final String queryDeleteRelationConfirmed = """
-            UPDATE FRIENDS_RELATIONS
-            SET RELATION_STATUS = 2
-            WHERE user_id = ?;
-            """;
-    private static final String queryDeleteRelation = """
-            DELETE FROM FRIENDS_RELATIONS
-            WHERE user_id = ? AND RELATION_STATUS = 1
-            OR FRIEND_ID = ? AND RELATION_STATUS = 2;
-            """;
     private final JdbcTemplate jdbcTemplate;
-
-    private long insertMessage(String query, User user) {
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-
-        jdbcTemplate.update(con -> {
-            PreparedStatement ps = con.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
-            ps.setString(1, user.getEmail());
-            ps.setString(2, user.getLogin());
-            ps.setString(3, user.getName());
-            ps.setString(4, String.valueOf(user.getBirthday()));
-
-            return ps;
-        }, keyHolder);
-        return Objects.requireNonNull(keyHolder.getKey()).longValue();
-    }
 
     @Override
     public void createRelation(Long userId, Long friendId, Long relationStatus) {
-        jdbcTemplate.update(queryCreateRelation, userId, friendId, relationStatus);
+        jdbcTemplate.update(QUERY_CREATE_RELATION, userId, friendId, relationStatus);
     }
 
     @Override
     public void deleteRelation(Long userId, Long friendId) {
-        jdbcTemplate.update(queryDeleteRelation, userId, friendId);
-        jdbcTemplate.update(queryDeleteRelationConfirmed, userId);
+        jdbcTemplate.update(QUERY_DELETE_RELATION, userId, friendId);
+        jdbcTemplate.update(QUERY_DELETE_RELATION_CONFIRMED, userId);
     }
 
     @Override
     public List<Relation> findFriendsByUserId(Long userId) {
-        String query = """
-                SELECT id, user_id, FRIEND_ID, RELATION_STATUS
-                FROM FRIENDS_RELATIONS
-                WHERE (user_id = ? AND (RELATION_STATUS = 1 OR RELATION_STATUS = 3))
-                UNION
-                SELECT id, user_id, FRIEND_ID, RELATION_STATUS
-                FROM FRIENDS_RELATIONS
-                WHERE (FRIEND_ID = ? AND (RELATION_STATUS = 2 OR RELATION_STATUS = 3))
-                """;
-        return jdbcTemplate.query(query, new RelationRowMapper(), userId, userId);
+        return jdbcTemplate.query(QUERY_FIND_FRIENDS_FOR_USER, new RelationRowMapper(), userId, userId);
     }
 
     @Override
     public List<Relation> findCommonFriends(Long firstUserId, Long secondUserId) {
-        String query = """
-                SELECT id, user_id, FRIEND_ID, RELATION_STATUS
-                FROM FRIENDS_RELATIONS
-                WHERE (user_id = ? AND (RELATION_STATUS = 1 OR RELATION_STATUS = 3))
-                UNION
-                SELECT id, user_id, FRIEND_ID, RELATION_STATUS
-                FROM FRIENDS_RELATIONS
-                WHERE (FRIEND_ID = ? AND (RELATION_STATUS = 2 OR RELATION_STATUS = 3))
-                INTERSECT
-                SELECT id, user_id, FRIEND_ID, RELATION_STATUS
-                FROM FRIENDS_RELATIONS
-                WHERE (user_id = ? AND (RELATION_STATUS = 1 OR RELATION_STATUS = 3))
-                UNION
-                SELECT id, user_id, FRIEND_ID, RELATION_STATUS
-                FROM FRIENDS_RELATIONS
-                WHERE (FRIEND_ID = ? AND (RELATION_STATUS = 2 OR RELATION_STATUS = 3))
-                """;
-        return jdbcTemplate.query(query, new RelationRowMapper(), firstUserId, firstUserId, secondUserId, secondUserId);
+        return jdbcTemplate.query(QUERY_FIND_RELATIONS_FOR_USERS, new RelationRowMapper(),
+                firstUserId, firstUserId, secondUserId, secondUserId);
     }
 
     @Override
     public List<Relation> findRelationByUserIds(Long userId, Long friendId) {
-        String query = "SELECT id, user_id, friend_id, relation_status FROM FRIENDS_RELATIONS WHERE (user_id = ? OR user_id = ?) AND (friend_id = ? OR friend_id = ?)";
-        return jdbcTemplate.query(query, new RelationRowMapper(), userId, friendId, userId, friendId);
+        return jdbcTemplate.query(QUERY_FIND_RELATION_ON_USERS_IDS, new RelationRowMapper(),
+                userId, friendId, userId, friendId);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/dal/impl/H2DatabaseUserDaoImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/impl/H2DatabaseUserDaoImpl.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.dal.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
@@ -17,6 +16,8 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
+import static ru.yandex.practicum.filmorate.dal.impl.query.UserQuery.*;
+
 @Slf4j
 @Repository("h2User")
 @RequiredArgsConstructor
@@ -24,103 +25,61 @@ public class H2DatabaseUserDaoImpl implements UserDao {
 
     private final JdbcTemplate jdbcTemplate;
 
-    public static String queryCreateUser = """
-            INSERT INTO users (email, login, name, birthday)
-            VALUES (?, ?, ?, ?);
-            """;
-
-    public static String queryUpdateUser = """
-            UPDATE users
-            SET email    = ?,
-                login    = ?,
-                name     = ?,
-                birthday = ?
-            WHERE user_id = ?;
-            """;
-
-
     @Override
     public Collection<User> findAll() {
-        String query = "SELECT user_id, email, login, name, birthday FROM users";
-        return jdbcTemplate.query(query, new UserRowMapper());
+        return jdbcTemplate.query(QUERY_FIND_ALL, new UserRowMapper());
     }
 
     @Override
     public User createUser(User user) {
-        long key = insertMessage(queryCreateUser, user);
+        long key = insertMessage(user);
         user.setId(key);
         return user;
     }
 
-    private long insertMessage(String query, User user) {
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-
-        jdbcTemplate.update(con -> {
-            PreparedStatement ps = con.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
-            ps.setString(1, user.getEmail());
-            ps.setString(2, user.getLogin());
-            ps.setString(3, user.getName());
-            ps.setString(4, String.valueOf(user.getBirthday()));
-
-            return ps;
-        }, keyHolder);
-        return Objects.requireNonNull(keyHolder.getKey()).longValue();
-    }
-
-    private long updateMessage(String query, User user) {
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-
-        jdbcTemplate.update(con -> {
-            PreparedStatement ps = con.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
-            ps.setString(1, user.getEmail());
-            ps.setString(2, user.getLogin());
-            ps.setString(3, user.getName());
-            ps.setString(4, String.valueOf(user.getBirthday()));
-            ps.setString(5, String.valueOf(user.getId()));
-
-            return ps;
-        }, keyHolder);
-        return Objects.requireNonNull(keyHolder.getKey()).longValue();
-    }
-
     @Override
     public User updateUser(User user) {
-        long key = updateMessage(queryUpdateUser, user);
+        long key = updateMessage(user);
         user.setId(key);
         return user;
     }
 
     @Override
     public Map<String, String> deleteUserById(Long id) {
-        String query = """
-                DELETE FROM users
-                WHERE user_id = :user_id;
-                """;
-        MapSqlParameterSource params = new MapSqlParameterSource();
-        params.addValue("user_id", id);
-        jdbcTemplate.update(query, params);
-
+        jdbcTemplate.update(QUERY_DELETE_USER_BY_ID, id);
         return Map.of("description", String.format("Пользоавтель с идентификатором: %d успешно удален.", id));
     }
 
     @Override
     public Optional<User> findUserById(Long id) {
-        String query = "SELECT user_id, email, login, name, birthday FROM users WHERE user_id = ?";
-        MapSqlParameterSource params = new MapSqlParameterSource();
-        params.addValue("user_id", id);
-        return Optional.ofNullable(jdbcTemplate.queryForObject(query, new UserRowMapper(), id));
+        return Optional.ofNullable(jdbcTemplate.queryForObject(QUERY_FIND_USER_BY_ID, new UserRowMapper(), id));
     }
 
-//    @Override
-//    public List<User> findUserById(Long id) {
-//        String query = """
-//                SELECT user_id, email, login, name, birthday
-//                FROM users
-//                WHERE user_id = :user_id;
-//                """;
-//        MapSqlParameterSource params = new MapSqlParameterSource();
-//        params.addValue("user_id", id);
-//
-//        return jdbcTemplate.query(query, (PreparedStatementSetter) params, new UserRowMapper());
-//    }
+    private long insertMessage(User user) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(QUERY_CREATE_USER, PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getEmail());
+            ps.setString(2, user.getLogin());
+            ps.setString(3, user.getName());
+            ps.setString(4, String.valueOf(user.getBirthday()));
+            return ps;
+        }, keyHolder);
+        return Objects.requireNonNull(keyHolder.getKey()).longValue();
+    }
+
+    private long updateMessage(User user) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(QUERY_UPDATE_USER,
+                    PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getEmail());
+            ps.setString(2, user.getLogin());
+            ps.setString(3, user.getName());
+            ps.setString(4, String.valueOf(user.getBirthday()));
+            ps.setString(5, String.valueOf(user.getId()));
+            return ps;
+        }, keyHolder);
+        return Objects.requireNonNull(keyHolder.getKey()).longValue();
+    }
 }
