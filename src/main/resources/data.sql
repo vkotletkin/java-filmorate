c1a7cba1e1e4fe74c325d25d2f5f174f2cb6cef0
Vladislav Kotletkin <drzhkn1@mail.ru>
2025-02-14 22:58:19 +0300

    fix: data.sql add delete

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,21 +1,13 @@
+DELETE FROM GENRES;
 INSERT INTO GENRES (genre_id, genre_name)
-VALUES (1, 'Комедия');
+VALUES (1, 'Комедия'),
+       (2, 'Драма'),
+       (3, 'Мультфильм'),
+       (4, 'Триллер'),
+       (5, 'Документальный'),
+       (6, 'Боевик');
 
-INSERT INTO GENRES (genre_id, genre_name)
-VALUES (2, 'Драма');
-
-INSERT INTO GENRES (genre_id, genre_name)
-VALUES (3, 'Мультфильм');
-
-INSERT INTO GENRES (genre_id, genre_name)
-VALUES (4, 'Триллер');
-
-INSERT INTO GENRES (genre_id, genre_name)
-VALUES (5, 'Документальный');
-
-INSERT INTO GENRES (genre_id, genre_name)
-VALUES (6, 'Боевик');
-
+DELETE FROM MPA;
 INSERT INTO MPA (MPA_ID, name)
 VALUES (1, 'G');
 
@@ -31,6 +23,7 @@ VALUES (4, 'R');
 INSERT INTO MPA (MPA_ID, name)
 VALUES (5, 'NC-17');
 
+DELETE FROM RELATION_STATUS;
 INSERT INTO RELATION_STATUS (STATUS_ID, STATUS)
 VALUES (1, 'Request Sender By First User');
 
